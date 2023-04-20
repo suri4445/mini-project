46489a4407e43672c33ec354e13220fe7507dd42
suri4445 <81067591+suri4445@users.noreply.github.com>
2023-04-20 09:21:09 +0530

    Adding Basic Components

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css';
+
+export const Header = () => {
+  return (
+    <div className="Title">
+      <h1>SFPOPOS</h1>
+            <div className="Title-Subtitle">San Franciscos Privately Owned Public Spaces</div>
+    </div>
+  )
+}
+
